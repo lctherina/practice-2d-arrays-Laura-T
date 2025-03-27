aa3eeb48e62c5480f38a6eb3f8339df5c32b7d7e
Laura Therina <lctherina@gmail.com>
2025-03-27 18:40:02 -0500

    Added commas after inner arrays.

--- practice2dArrays.js
+++ practice2dArrays.js
@@ -1,19 +1,19 @@
 let allScores = [
-    [76, 82, 94]
-    [92, 94, 91]
+    [76, 82, 94],
+    [92, 94, 91],
     [83, 87, 82]
 ];
 
 
 let theaterSeating = [
-    ["A1", "A2", "A3", "A4"]
-    ["B1", "B2", "B3", "B4"]
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
     ["C1", "C2", "C3", "C4"]
 ];
 
 
 let contactsList = [
-    ["Sarah Smith", "ssmith26@smithmail.com", "555-345-7890"]
+    ["Sarah Smith", "ssmith26@smithmail.com", "555-345-7890"],
     ["John Johnson", "jjohnson@johnmail.com", "555-123-4567"]
 ];
 
